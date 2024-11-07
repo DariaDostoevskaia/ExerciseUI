daa21016526ab4da7c3572c171cdebb79e2c0e6f
DariaDostoevskaia <dar.dost789@gmail.com>
2024-11-08 02:08:48 +0300

    fix: inventory so

--- ExerciseUI/Assets/Scripts/ScriptalObjects/InventorySO.cs
+++ ExerciseUI/Assets/Scripts/ScriptalObjects/InventorySO.cs
@@ -5,5 +5,11 @@ namespace ExerciseUI.ScriptalObjects
     [CreateAssetMenu(fileName = nameof(InventorySO), menuName = "Config/InventorySO")]
     public class InventorySO : ScriptableObject
     {
+        [SerializeField] private string _title;
+        [SerializeField] private string _text;
+
+        [SerializeField] private int _count = 1;
+
+        public int JumpLenght => _count;
     }
 }
